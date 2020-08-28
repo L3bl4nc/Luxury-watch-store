6585a82d639d3a416a10adac277d65550e10d807
Sarin Boivinpicard <sarinbp@gmail.com>
2020-08-27 20:19:30 -0400

    added index show create methods

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,2 +1,30 @@
 class WatchesController < ApplicationController
+	def index
+		@watches = Watch.all
+	end
+	
+	def show
+		#(params[:id])
+		@watch = Watch.find(params[:id])
+	end
+
+	def new
+		@watch = Watch.new
+	end
+
+	def create
+		@watch = Watch.new(watch_params)
+		if condition
+			
+		end
+		
+		@watch.save
+	end
+	
+	private
+
+	def watch_params
+		params.require(:watch).permit(:brand, :model_year, :price, :condition, :reference_number, :photo)
+	end
+	
 end
