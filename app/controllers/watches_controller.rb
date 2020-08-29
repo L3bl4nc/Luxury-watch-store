2e7eeb093c2f887682f4fdb35ec0382b7229cc33
Nikolaus Libby <niktimekeeper5@gmail.com>
2020-08-29 12:05:10 -0500

    pushing the form

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,2 +1,28 @@
 class WatchesController < ApplicationController
+
+  def index
+    @watches = Watch.all
+  end
+
+  def show
+    @watch = Watch.find(params[:id])
+  end
+
+  def new
+    @watch = Watch.new
+  end
+
+  def create
+    @watch = Watch.find(watch_params)
+    @watch.save
+
+    redirect_to watches_path
+
+  end
+
+  private
+
+  def watch_params
+    params.require(:watch).permit(:brand, :reference_number, :model_year, :price, :condition)
+  end
 end
