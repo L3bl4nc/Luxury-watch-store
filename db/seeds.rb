79f6b9f3e5536bae739415b082f369a2aeb949cc
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-08-29 12:12:15 -0400

    modified card design and added some watches in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,16 @@ puts "Creating some data"
 
 reference_number = ['7463.44.67', '3456.42.89', '3546.98.41']
 
-user_vincent = User.find(1)
+user_test = User.find(1)
 
-3.times do 
+10.times do 
   watch = Watch.new(brand: 'Rolex',
                     model_year: 1989, 
                     price: 10000, 
                     condition: 'Prestine',
                     reference_number: reference_number.sample,
                    )
-  watch.user = user_vincent
+  watch.user = user_test
   watch.save
 end
 
