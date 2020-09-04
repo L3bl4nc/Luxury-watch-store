108ad80147a1d0a24d56a781c53710961cf3256c
Vincent Leblanc <vincent.leblanc1990@gmail.com>
2020-09-03 20:36:52 -0400

    changed a bit seed file and added icon for tab/browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-puts "Cleaning the database"
+puts "Cleaning the database..."
 Watch.destroy_all
 User.destroy_all
-puts "Creating some data"
 
-reference_number = ['7463.44.67', '3456.42.89', '3546.98.41']
+puts "Database clean!"
+
+puts "Creating some data..."
+
+watches_brand
+references_number = ['7463.44.67', '3456.42.89', '3546.98.41', '4533.23.49', '8265.04.91']
 
 user_test = User.create(email: "test@gmail.com", password: "123456")
 
@@ -12,7 +16,7 @@ user_test = User.create(email: "test@gmail.com", password: "123456")
                     model_year: 1989, 
                     price: 10000, 
                     condition: 'Pristine',
-                    reference_number: reference_number.sample,
+                    reference_number: references_number.sample,
                    )
   watch.user = user_test
   watch.save
